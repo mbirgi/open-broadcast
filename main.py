65c97ab527cfde7adbdf3cf88ba32fb498a8afda
memo <mbirgi@gmail.com>
2025-01-04 15:59:50 +0100

    Remove unused import for spotipy to clean up code

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import time
-import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv
 import os
